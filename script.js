e7a8984875d43a7245f111f0c22b9303df4b8c93
The KinG Of Rich <batuocbongdem156@gmail.com>
2026-07-08 09:50:57 +0700

    v1.0

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-// Khi load trang, thêm class để kick các hiệu ứng animate từng dòng
 window.addEventListener("DOMContentLoaded", () => {
   document.querySelectorAll('.fade-in, .fade-in-right').forEach(el => {
     el.style.animationPlayState = 'running';
   });
+
+  const bgImages = [
+    'img_body/1.jpg',
+    'img_body/1.png'
+  ];
+
+  const bgSlider = document.querySelector('.bg-slider');
+  let currentBgIndex = 0;
+
+  if (bgSlider && bgImages.length > 0) {
+    bgSlider.style.backgroundImage = `url('${bgImages[currentBgIndex]}')`;
+    setInterval(() => {
+      currentBgIndex = (currentBgIndex + 1) % bgImages.length;
+      bgSlider.style.backgroundImage = `url('${bgImages[currentBgIndex]}')`;
+    }, 3000);
+  }
 });
